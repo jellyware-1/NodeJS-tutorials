722a42c59fa271bc7f3acbaa7096d073b013a06a
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-05-31 18:43:31 +0100

    testing if the server loads html

--- myApp.js
+++ myApp.js
@@ -5,7 +5,16 @@ let app = express();
 console.log("Hello World");
 
 app.get("/", function(req, res){
-    res.send("Hello Express");
+    console.log("inside the response");
+    res.send(`
+
+    <html>
+        <body style="text-align: center;">
+            <h1 style="font-family: 'Courier New', Courier, monospace; font-size: 50px;">HELLO EXPRESS</h1>
+        </body>
+    </html>
+    
+    `);
 });
 
 
