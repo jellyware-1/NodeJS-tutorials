cd25632aabe25789e83c45cb23d8481596446e6a
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-05-31 19:28:25 +0100

    challenge 3

--- myApp.js
+++ myApp.js
@@ -1,51 +1,6 @@
 let express = require('express');
 let app = express();
 
-
-console.log("Hello World");
-
-app.get("/", function(req, res){
-    console.log("inside the response");
-    res.send(`
-
-    <html>
-        <body style="text-align: center;">
-            <h1 style="font-family: 'Courier New', Courier, monospace; font-size: 50px;">HELLO EXPRESS</h1>
-        </body>
-    </html>
-    
-    `);
-});
-
-console.log("after get function")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", (req, res) => res.send(__dirname + "/views/index.html"));
 
  module.exports = app;
