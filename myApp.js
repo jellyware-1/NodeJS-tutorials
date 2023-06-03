e2f41e8834a7ba7338f835b043f6dcb7ccfaf9fa
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-06-03 18:11:02 +0100

    changed

--- myApp.js
+++ myApp.js
@@ -7,15 +7,14 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 app.use("/public", express.static(__dirname + "/public"));
 
-console.log(process.env.MESSAGE_STYLE);
-
 app.get("/json", (req, res)=>{
     let message = "Hello json";
     if(process.env.MESSAGE_STYLE == "uppercase"){
-        res.json(message.toUpperCase());
+        message = message.toUpperCase();
+        res.json({message});
     }
     else{
-        res.json(message);
+        res.json({message});
         
     }
 });
