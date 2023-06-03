0e951159d113dcdda596d1b89d74cdde9921e366
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-06-03 18:24:18 +0100

    next challenege

--- myApp.js
+++ myApp.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path  + " - " + req.ip);
+    next();
+})
+
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 app.use("/public", express.static(__dirname + "/public"));
