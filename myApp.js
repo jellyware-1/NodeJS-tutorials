0c61e5867f95a23b987ff8ba9008ef638901552d
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-06-03 19:24:17 +0100

    challenge 9

--- myApp.js
+++ myApp.js
@@ -3,15 +3,22 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
+//works on all request - logs method, path accessed + IP address of requester
 app.use((req, res, next) => {
     console.log(req.method + " " + req.path  + " - " + req.ip);
     next();
 })
 
+//sends file when at root
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
+
+//middle ware - when public accesed - sends contents of static file
 app.use("/public", express.static(__dirname + "/public"));
 
+//defining middle ware and handler in same route definition
+//attaches the time to the request
+//sends the time in json format
 app.get("/now", function(req, res, next){
     req.time = (new Date()).toString();
     next();
@@ -20,6 +27,8 @@ app.get("/now", function(req, res, next){
 }
 )
 
+//environment variables
+//output upper case depending on the environemtn variables set
 app.get("/json", (req, res)=>{
     let message = "Hello json";
     if(process.env.MESSAGE_STYLE == "uppercase"){
@@ -31,5 +40,9 @@ app.get("/json", (req, res)=>{
         
     }
 });
+
+app.get("/:word/echo", function(req, res){
+    res.json({"echo": req.params.word});
+})
 
 module.exports = app;
