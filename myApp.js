e57a901b3ce8c4b671e7464074862929349f6ccc
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-06-03 19:15:01 +0100

    challenge 8

--- myApp.js
+++ myApp.js
@@ -12,6 +12,14 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/now", function(req, res, next){
+    req.time = (new Date()).toString();
+    next();
+}, function(req, res){
+    res.json({"time": req.time});
+}
+)
+
 app.get("/json", (req, res)=>{
     let message = "Hello json";
     if(process.env.MESSAGE_STYLE == "uppercase"){
